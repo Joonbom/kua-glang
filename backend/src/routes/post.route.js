9600521bf76844d41f9139c090ab977fdcbaedab
Pattayawat <pattayawat.poo@dome.tu.ac.th>
2025-05-24 16:29:03 +0700

    finish commu

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -4,9 +4,19 @@ const router = express.Router();
 const createPost = require("../controllers/post/post.controller")
 const updatePost = require("../controllers/post/updatePost.controller")
 const deletePost = require("../controllers/post/deletePost.controller")
+const getAllPosts = require("../controllers/post/getAllPosts.controller")
+const getPostsFromFollowing = require("../controllers/post/getPostsFromFollowing.controller")
+const likePost = require("../controllers/post/likePost.controller")
+const commentPost = require("../controllers/post/commentPost.controller")
+const likeComment = require("../controllers/post/likeComment.contoller")
 
+router.get('/', getAllPosts);
+router.get('/:userId', getPostsFromFollowing);
 router.post('/:userId', createPost);
 router.put('/:userId/:postId', updatePost);
 router.delete('/:userId/:postId', deletePost);
+router.post('/like/:userId/:postId', likePost);
+router.post('/post/:postId/comment/:userId', commentPost);
+router.post('/post/:cid/comment/:userId', likeComment);
 
 module.exports = router;
