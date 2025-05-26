7f09c954f9b91bf41bd88d390ae16736dc8254aa
tanespol <tanespol.pra@dome.tu.ac.th>
2025-05-26 22:02:12 +0700

    mek

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage/HomePage.jsx';
-import AddFolder from './components/AddFolder/AddFolder.jsx';
-import RecipeDetail from './components/RecipeDetail/RecipeDetail.jsx';
-import CreateRecipe from './components/CreateRecipe/CreateRecipe.jsx';
-import EditIngredient from './components/EditIngredient/EditIngredient.jsx';
-import ProfilePage from './pages/ProfilePage/ProfilePage.jsx';
-import EditProfilePage from './pages/ProfilePage/EditProfilePage.jsx';
-import EditFieldPage from './pages/ProfilePage/EditFieldPage.jsx';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage.jsx";
+import AddFolder from "./components/AddFolder/AddFolder.jsx";
+import RecipeDetail from "./components/RecipeDetail/RecipeDetail.jsx";
+import CreateRecipe from "./components/CreateRecipe/CreateRecipe.jsx";
+import EditIngredient from "./components/EditIngredient/EditIngredient.jsx";
+import ProfilePage from "./pages/ProfilePage/ProfilePage.jsx";
+import EditProfilePage from "./pages/ProfilePage/EditProfilePage.jsx";
+import EditFieldPage from "./pages/ProfilePage/EditFieldPage.jsx";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home"; //  import หน้าใหม่
-import CommunityPage from './pages/CommunityPage/CommunityPage.jsx';
-import CreatePostPage from './pages/CreatePostPage/CreatePostPage.jsx';
-import EditPostPage from './pages/EditPostPage/EditPostPage.jsx';  
-import FollowingPage from './pages/FollowingPage/FollowingPage.jsx'; 
-import EditProfileImagePage from './pages/ProfilePage/EditProfileImage.jsx';
-import Competition from './pages/Competition.jsx';
-
+import CommunityPage from "./pages/CommunityPage/CommunityPage.jsx";
+import CreatePostPage from "./pages/CreatePostPage/CreatePostPage.jsx";
+import EditPostPage from "./pages/EditPostPage/EditPostPage.jsx";
+import FollowingPage from "./pages/FollowingPage/FollowingPage.jsx";
+import Competition from "./pages/Competition.jsx";
+import HistoryScreen from "./components/History/History.jsx";
+import AddShareItemScreen from "./components/AddShareItem/AddShareItem.jsx";
+import ShareScreen from "./components/Share/Share.jsx";
 function App() {
   return (
     <Routes>
@@ -28,7 +29,8 @@ function App() {
       <Route path="/profile" element={<ProfilePage />} />
       <Route path="/edit-profile" element={<EditProfilePage />} />
       <Route path="/edit/:field" element={<EditFieldPage />} />
-      <Route path="/competition" element={<Competition />} /> {/* หน้าระดับ/แงค์ */}
+      <Route path="/competition" element={<Competition />} />{" "}
+      {/* หน้าระดับ/แงค์ */}
       {/* เพิ่ม Route อื่นๆ ตามต้องการ */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
@@ -37,7 +39,9 @@ function App() {
       <Route path="/community/create" element={<CreatePostPage />} />
       <Route path="/community/edit/:postId" element={<EditPostPage />} />
       <Route path="/community/following" element={<FollowingPage />} />
-      <Route path="/edit/profile_url" element={<EditProfileImagePage />} />
+      <Route path="/history" element={<HistoryScreen />} />
+      <Route path="/add-share-item" element={<AddShareItemScreen />} />
+      <Route path="/share" element={<ShareScreen />} />
     </Routes>
   );
 }
